da716bb5bd9fc799c6aabc9ac19805ad96b590da
Pranav Hegde <ahpranav12@gmail.com>
2024-02-26 15:33:31 +0530

    created a .so file and linked to executable

---
+++ library/f.cpp
@@ -0,0 +1,6 @@
+#include "f.h"
+#include <iostream>
+
+void myFunction() {
+    std::cout << "Hello from myFunction!" << std::endl;
+}
---
+++ main/main.cpp
@@ -0,0 +1,8 @@
+// main.cpp
+
+#include "f.h" // Include the header file for the shared library
+
+int main() {
+    myFunction(); // Call a function from the shared library
+    return 0;
+}
